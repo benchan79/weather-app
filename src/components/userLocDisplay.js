f14b8e36910dcae9ba26d748c5c3ed74e261032d
Ben Chan <benchan79@gmail.com>
2023-01-15 21:55:47 +0800

    Add units context to userLocDisplay

--- src/components/userLocDisplay.js
+++ src/components/userLocDisplay.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { geoAPIGetByCoords } from "../services/geoAPI";
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import styles from './userLocDisplay.module.css'
+import WeatherContext from "../contexts/WeatherContext";
 
 function UserLocDisplay({ onClick }) {
+  const ctx = useContext(WeatherContext);
   const [userLocation, setUserlocation] = useState(
     {
       lat: 1.3521,
@@ -27,7 +29,7 @@ function UserLocDisplay({ onClick }) {
             lat: position.coords.latitude,
             long: position.coords.longitude,
           }
-          geoAPIGetByCoords(coords, onClick);
+          geoAPIGetByCoords(coords, onClick, ctx.isMetric);
         }
         catch (error) {
           console.error(error);
