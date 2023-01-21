ff0c891e5d62641fcab27f34e066581ba669620d
Ben Chan <benchan79@gmail.com>
2023-01-21 08:33:47 +0800

    Remove extra line

--- src/components/UnitsSelector.js
+++ src/components/UnitsSelector.js
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import { useContext } from "react";
 import WeatherContext from "../contexts/WeatherContext";
 
 function UnitsSelector({ searchParam, getSearchData }) {
